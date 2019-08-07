3cc98d17ad9afc2b080df2f1cf51436e30fb97f2
kuswandanu <kuswandanu@gmail.com>
2019-08-07 19:11:34 +0700

    Add analyze image

--- config.js
+++ config.js
@@ -3,6 +3,12 @@ const _module = {
     getStorageAccountName: () => {
         const matches = /AccountName=(.*?);/.exec(process.env.AZURE_STORAGE_CONNECTION_STRING);
         return matches[1];
+    },
+    getCognitiveServicesEndpoint: () => {
+        return process.env.AZURE_COGNITIVE_SERVICES_ENDPOINT;
+    },
+    getCognitiveServicesKey: () => {
+        return process.env.AZURE_COGNITIVE_SERVICES_KEY;
     }
 };
 
--- routes/detail.js
+++ routes/detail.js
@@ -7,18 +7,48 @@ const
     , router = express.Router()
     , containerName = 'images'
     , config = require('../config')
+    , request = require('request')
 ;
 
 router.get('/:fileName', (req, res, next) => {
 
     const fileName = req.params["fileName"];
 
-    res.render('detail', { 
-        title: fileName,
-        image: fileName,
-        accountName: config.getStorageAccountName(),
-        containerName: containerName,
-        result: ""
+    const options = {
+        uri: config.getCognitiveServicesEndpoint(),
+        qs: {
+            'visualFeatures': 'Categories,Description,Color',
+            'details': '',
+            'language': 'en'
+        },
+        body: '{"url": ' + '"https://' + config.getStorageAccountName() + '.blob.core.windows.net/' + containerName + '/' + fileName + '"}',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': config.getCognitiveServicesKey()
+        }
+    };
+
+    request.post(options, (error, response, body) => {
+        if (error) {
+            res.status(500);
+
+            res.render('error', {
+                title: 'Error',
+                message: 'There was an error connecting to cognitive services.',
+                error: error
+            });
+        }
+        else {
+            let jsonResponse = JSON.stringify(JSON.parse(body), null, 2);
+
+            res.render('detail', { 
+                title: fileName,
+                image: fileName,
+                accountName: config.getStorageAccountName(),
+                containerName: containerName,
+                result: jsonResponse
+            });
+        }
     });
 
 });
